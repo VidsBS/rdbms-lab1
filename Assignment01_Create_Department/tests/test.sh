5898b237650e2ce75ad9b22865abc616fb0f1981
VidsBS <141306168+VidsBS@users.noreply.github.com>
2026-07-16 23:16:21 -0700

    Update test.sh

--- Assignment01_Create_Department/tests/test.sh
+++ Assignment01_Create_Department/tests/test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+echo "Running solution.sql"
 
 mysql -h127.0.0.1 -P3306 -uroot -proot < solution.sql
+
+echo "Showing databases"
+
+mysql -h127.0.0.1 -P3306 -uroot -proot -e "SHOW DATABASES;"
+
+echo "Using CollegeDB"
 
 mysql -h127.0.0.1 -P3306 -uroot -proot -e "
 USE CollegeDB;
 SHOW TABLES;
-" > output.txt
-
-grep -q "Department" output.txt
+"
 
 echo "PASS"
