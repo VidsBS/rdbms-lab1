c3d3918602ec4067ba2190ca2fd71d2752df2ffb
VidsBS <141306168+VidsBS@users.noreply.github.com>
2026-07-16 23:13:18 -0700

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,7 +1,8 @@
 -- Write your SQL here
 
 -- Create Database
-
+create database collegeDB;
 -- Use Database
-
+use collegeDB;
 -- Create Department Table
+create Table department(department_id int primary key, department_name varchar(10), hod varchar(10));
