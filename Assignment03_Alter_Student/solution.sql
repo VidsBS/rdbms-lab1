220e7063555f8b1d862a16b732ce4f8091473d0e
VidsBS <141306168+VidsBS@users.noreply.github.com>
2026-07-16 23:27:59 -0700

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,6 +1,21 @@
+-- Remove existing database (optional for repeatable testing)
 DROP DATABASE IF EXISTS CollegeDB;
+
+-- Create database
 CREATE DATABASE CollegeDB;
+
+-- Select database
 USE CollegeDB;
+
+-- Create Student table
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE,
+    Gender VARCHAR(10),
+    DepartmentID INT(5)
+);
+
 -- Alter Student table
 
 -- Add Email
