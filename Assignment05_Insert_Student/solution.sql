b434b009c6d5e6700a3fe76179c1eee4fc1c4dbe
VidsBS <141306168+VidsBS@users.noreply.github.com>
2026-07-16 23:29:37 -0700

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,3 +1,17 @@
+DROP DATABASE IF EXISTS CollegeDB;
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
+
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE,
+    Gender VARCHAR(10),
+    DepartmentID INT(5),
+    Email VARCHAR(30),
+    PhoneNumber BIGINT
+);
+
 -- Insert
 
 -- 1001 Arun
